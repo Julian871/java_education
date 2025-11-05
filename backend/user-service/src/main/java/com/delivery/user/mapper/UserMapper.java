01b0906ea5af7aaf3dc9f57cab669a095957508e
Julian <julian_medvedev99@mail.ru>
2025-11-05 16:40:46 +0300

    fix: delete unnecessary functions

--- backend/user-service/src/main/java/com/delivery/user/mapper/UserMapper.java
+++ backend/user-service/src/main/java/com/delivery/user/mapper/UserMapper.java
@@ -8,30 +8,19 @@ import com.delivery.user.entity.Address;
 import com.delivery.user.entity.Role;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.factory.Mappers;
 
 import java.util.Set;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
-
     UserDto toDto(User user);
-
-    User toEntity(UserDto userDto);
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "user", ignore = true)
-    void updateAddressFromDto(AddressDto addressDto, @MappingTarget Address address);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "user", ignore = true)
     Address toEntity(AddressDto addressDto);
 
     Set<AddressDto> toAddressDtoSet(Set<Address> addresses);
-    Set<Address> toAddressSet(Set<AddressDto> addressDtos);
 
     RoleDto toDto(Role role);
 }
