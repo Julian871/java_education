69d05dde7ed403fe50f4849d6873e2434ac3130d
Julian <julian_medvedev99@mail.ru>
2025-11-07 14:08:09 +0300

    fix: add lombok constructor

--- backend/user-service/src/main/java/com/delivery/user/exception/ApiException.java
+++ backend/user-service/src/main/java/com/delivery/user/exception/ApiException.java
@@ -1,14 +1,12 @@
 package com.delivery.user.exception;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@RequiredArgsConstructor
 public class ApiException extends RuntimeException {
+    private final String message;
     private final HttpStatus status;
-
-    public ApiException(String message, HttpStatus status) {
-        super(message);
-        this.status = status;
-    }
 }
